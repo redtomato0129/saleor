0e71baf69ede733dfae1842e5b102dd834f32a00
superman030129 <superman030129@gmail.com>
2024-03-28 08:29:59 -0400

    change superman

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -85,12 +85,9 @@ export async function Footer({ channel }: { channel: string }) {
 					<p className="text-sm text-neutral-500">Copyright &copy; {currentYear} Your Store, Inc.</p>
 					<p className="flex gap-1 text-sm text-neutral-500">
 						Powered by{" "}
-						<Link target={"_blank"} href={"https://saleor.io/"}>
-							Saleor
+						<Link target={"_blank"} href={""}>
+							SuperMan
 						</Link>{" "}
-						<Link href={"https://github.com/saleor/saleor"} target={"_blank"} className={"opacity-30"}>
-							<Image alt="Saleor github repository" height={20} width={20} src={"/github-mark.svg"} />
-						</Link>
 					</p>
 				</div>
 			</div>
